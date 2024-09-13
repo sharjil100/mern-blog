8d9ce2bfb5f90b2a08e59b5435b058a350235cb7
Sharjil Khan <sharjilk73@gmail.com>
2024-09-13 20:36:36 +0600

    changed projects.jsx to Projects.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,27 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import SignUp from './pages/SignUp'
-import Signin from './pages/Signin'
-import Dashboard from './pages/Dashboard'
-import projects from './pages/projects'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import Signin from './pages/Signin';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
 
-
-import React from 'react'
+import React from 'react';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-      < Route path="/" element={<Home />}/>
-      < Route path="/about" element={<About />}/>
-      < Route path="/sign-in" element={<Signin />}/>
-      < Route path="/sign-up" element={<SignUp />}/>
-      < Route path="/dashboard" element={<Dashboard />}/>
-      < Route path="/project" element={<projects />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="*" element={<div>404 Not Found</div>} /> {/* Fallback route */}
       </Routes>
-      
     </BrowserRouter>
-  )
+  );
 }
