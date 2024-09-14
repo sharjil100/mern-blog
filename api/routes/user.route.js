52e8a0d48840e1cc5e71e08fff162703734b1428
Sharjil Khan <sharjilk73@gmail.com>
2024-09-15 04:15:29 +0600

    test api(fixed)

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import { test } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get('/test', (req,res) => {
-    res.json({message: 'API is working!'});
-});
+router.get('/test', test);
 
 //console.log('User route file loaded');
 
