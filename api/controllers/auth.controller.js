a53e46c486cf07b2c198dfa0b107cba06119fb3d
Sharjil Khan <sharjilk73@gmail.com>
2024-09-15 21:06:21 +0600

    create signup api routes complete

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async(req, res) => {
     const { username, email, password } = req.body;
@@ -10,12 +11,21 @@ export const signup = async(req, res) => {
         return res.status(400).json({message: 'All fields are required'});
      }
 
+     const hashedPassword = bcryptjs.hashSync(password, 10);
+
     const newUser = new User({
         username,
         email,
-        password,
+        password: hashedPassword,
     });
 
-    await newUser.save();
-    res.json({message:'Signup successful'});
+    try {
+        await newUser.save();
+        res.json({message:'Signup successful'});
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+
+    
 };
